3bbbbdeba834fb2cf098186a90f906179a46d901
cat-catttt <ckg24001@st.ckg.ac.jp>
2026-02-24 11:21:39 +0900

    STEP1 カレンダー月表示機能追加

--- app/main.py
+++ app/main.py
@@ -1,11 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 from .database import engine
 from . import models
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+templates = Jinja2Templates(directory="app/templates")
 
-@app.get("/")
-def root():
-    return {"message": "Environment OK"}
+@app.get("/", response_class=HTMLResponse)
+def index(request: Request):
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request}
+    )
